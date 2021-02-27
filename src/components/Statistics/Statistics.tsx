8afd8379332d136f0cfea334c8d594bb7dc5a549
Victor Litvinenko <v@cleanweb.ru>
2021-02-27 08:43:21 +0300

    style: repair layout

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -4,6 +4,7 @@ import 'moment/locale/ru';
 import * as Icon from 'react-feather';
 
 import { formatDuration } from '../../libs/utils';
+import rootStore from '../../stores/root-store';
 import settingsStore from '../../stores/settings/settings-store';
 import StatisticsStore from '../../stores/statistics/statistics-store';
 import Btn from '../Btn';
@@ -13,27 +14,34 @@ import './statistics.scss';
 moment.locale('ru');
 
 const Statistics: React.FC = () => {
+  const applyChanges = () => {
+    rootStore.DataStore.GridStore.init();
+    settingsStore.changeStatus('default');
+  };
+
   return (
     <div className="statistics">
       <div className="statistics__ui">
-        <Btn onClick={() => settingsStore.changeStatus('default')}>
+        <Btn onClick={applyChanges}>
           <Icon.ArrowLeft color="white" />
         </Btn>
       </div>
       <div className="settings__title">Статистика</div>
-      <table>
-        {StatisticsStore.items.map((item) => (
-          <tr>
-            <td>{moment(item.date).format('LLL')}, </td>
-            <td>{item.moves} ходов, </td>
-            <td>{formatDuration(item.time)}</td>
-          </tr>
-        ))}
-        {StatisticsStore.items.length === 0 && (
-          <tr>
-            <td>Нет данных</td>
-          </tr>
-        )}
+      <table cellSpacing={0}>
+        <tbody>
+          {StatisticsStore.items.map((item) => (
+            <tr key={item.date.toString()}>
+              <td>{moment(item.date).format('LLL')}</td>
+              <td>{item.moves} ходов</td>
+              <td>{formatDuration(item.time)}</td>
+            </tr>
+          ))}
+          {StatisticsStore.items.length === 0 && (
+            <tr>
+              <td>Нет данных</td>
+            </tr>
+          )}
+        </tbody>
       </table>
     </div>
   );
